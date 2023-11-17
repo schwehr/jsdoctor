d9b247422c309f2d1fd1a04c3118595a75324025
Kurt Schwehr <schwehr@google.com>
2023-11-17 11:15:28 -0800

    __init__.py: Do not need to explicitely import each module in the package

--- jsdoctor/__init__.py
+++ jsdoctor/__init__.py
@@ -1,10 +1,1 @@
 # Expose the modules of the package.
-
-from . import flags
-from . import generator
-from . import jsdoc
-from . import linkify
-from . import namespace
-from . import scanner
-from . import source
-from . import symboltypes
