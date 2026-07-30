abd06585fe03346bd95c30eb4b03f90c8adb1479
Kurt Schwehr <schwehr@google.com>
2026-07-29 21:43:04 -0700

    feat(init): re-export package submodules in __init__.py

--- jsdoctor/__init__.py
+++ jsdoctor/__init__.py
@@ -1,1 +1,23 @@
-# Expose the modules of the package.
+"""Core JSDoc extraction and HTML documentation generation library."""
+
+from jsdoctor import esprima
+from jsdoctor import flags
+from jsdoctor import generator
+from jsdoctor import jsdoc
+from jsdoctor import linkify
+from jsdoctor import namespace
+from jsdoctor import scanner
+from jsdoctor import source
+from jsdoctor import symboltypes
+
+__all__ = [
+    "esprima",
+    "flags",
+    "generator",
+    "jsdoc",
+    "linkify",
+    "namespace",
+    "scanner",
+    "source",
+    "symboltypes",
+]
